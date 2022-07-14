cda154cd7765854ca97cdec1ca1ad46b79e8ea32
Meisam Mofidi <meisam.mofidi@telus.com>
2022-07-14 14:31:39 -0400

    Add summary form unit tests

--- src/components/SummaryForm/SummaryForm.jsx
+++ src/components/SummaryForm/SummaryForm.jsx
@@ -1,19 +1,33 @@
 import React, { useState } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Popover, OverlayTrigger } from "react-bootstrap";
 
 const SummaryForm = () => {
   const [tcCheck, setTcCheck] = useState(false);
-  const tcLable = (
+
+  const popover = (
+    <Popover id="popover-basic">
+      <Popover.Content>
+        If not accept the term and conditions then no Ice cream will be
+        delivered
+      </Popover.Content>
+    </Popover>
+  );
+
+  const tcLabel = (
     <span>
-      I agree to <span style={{ color: "blue" }}> Term and Conditions</span>
+      I agree to
+      <OverlayTrigger trigger="hover" placement="right" overlay={popover}>
+        <span style={{ color: "blue" }}> Term and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
+
   return (
     <Form>
       <Form.Group controlId="formBasicCheckbox">
         <Form.Check
           type="checkbox"
-          label={tcLable}
+          label={tcLabel}
           checked={tcCheck}
           onChange={(e) => setTcCheck(e.target.checked)}
         />
--- src/components/SummaryForm/__tests__/SummaryForm.test.jsx
+++ src/components/SummaryForm/__tests__/SummaryForm.test.jsx
@@ -1,4 +1,9 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import SummaryForm from "../SummaryForm";
 
 test("should create snapshot", () => {
@@ -8,21 +13,53 @@ test("should create snapshot", () => {
 
 test("should initial condition", () => {
   render(<SummaryForm />);
-  const confirmOrderButton = screen.getByRole('button', { name: /confirm order/i });
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
   expect(confirmOrderButton).toBeDisabled();
 
-  const tcCheckbox = screen.getByRole('checkbox', { name: /term and conditions/i });
-  expect(tcCheckbox).not.toBeChecked()
-  
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /term and conditions/i,
+  });
+  expect(tcCheckbox).not.toBeChecked();
 });
 
 test("should enable confirm button on first click and disables on second click", () => {
   render(<SummaryForm />);
-  const confirmOrderButton = screen.getByRole('button', { name: /confirm order/i });
-  const tcCheckbox = screen.getByRole('checkbox', { name: /term and conditions/i });
+  const confirmOrderButton = screen.getByRole("button", {
+    name: /confirm order/i,
+  });
+  const tcCheckbox = screen.getByRole("checkbox", {
+    name: /term and conditions/i,
+  });
 
-  fireEvent.click(tcCheckbox);
+  userEvent.click(tcCheckbox);
   expect(confirmOrderButton).toBeEnabled();
-  fireEvent.click(tcCheckbox);
+  userEvent.click(tcCheckbox);
   expect(confirmOrderButton).toBeDisabled();
+});
+
+test("popover responds to hover", async () => {
+  render(<SummaryForm />);
+  // Popover starts out hidden
+  const nullPopover = screen.queryByText(
+    /If not accept the term and conditions then no Ice cream will be delivered/i
+  );
+  expect(nullPopover).not.toBeInTheDocument();
+
+  // Popover appears upon mouseover on checkbox label
+  const termAndConditions = screen.getByText(/term and conditions/i);
+  userEvent.hover(termAndConditions);
+  const popover = screen.getByText(
+    /If not accept the term and conditions then no Ice cream will be delivered/i
+  );
+  expect(popover).toBeInTheDocument();
+
+  // Popoover disappears when we mousse out
+  userEvent.unhover(termAndConditions);
+  await waitForElementToBeRemoved(() =>
+    screen.queryByText(
+      /If not accept the term and conditions then no Ice cream will be delivered/i
+    )
+  );
 });
